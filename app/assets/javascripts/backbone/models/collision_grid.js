268d146f1fe291351ddca133050ada952920fe94
variousauthors <various.authors@gmail.com>
2013-10-13 23:36:18 +0200

    Starting to fill in the collision_grid methods

--- app/assets/javascripts/backbone/models/collision_grid.js
+++ app/assets/javascripts/backbone/models/collision_grid.js
@@ -10,6 +10,10 @@ MyApp.Models.CollisionGrid = Backbone.Model.extend({
   },
 
   startTransaction: function () {
+    this.open_transactions.push({
+      origin: undefined, // where our search began
+      current: undefined // where we are now
+    });
 
   },
 
@@ -18,7 +22,22 @@ MyApp.Models.CollisionGrid = Backbone.Model.extend({
   },
 
   add: function (origin) {
+    if (this.open_transactions.length > 0) {
+      if (this.open_transactions.first.origin != undefined) {
+        return this._continueAdd(origin);
+      }
+    }
+
+    // find a free cell near the given point and flip it
+
     return origin;
+  },
+
+  _continueAdd: function (origin) {
+    // continue to spiral outward from the origin
+    // picking up wherever we left off
+    // in search of a free cell to flip
+
   },
 
   getLine: function (start, end, options) {
